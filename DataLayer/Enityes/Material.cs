c9b95353b5a7f4eddd5758e5ecc1246424894317
GitMaiter <mcmaiter@gmail.com>
2019-02-18 00:56:21 +0200

    Remove usings and empty spaces

--- DataLayer/Enityes/Material.cs
+++ DataLayer/Enityes/Material.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
 namespace DataLayer.Enityes
 {
     public class Material : Page
     {
-        public  int Id { get; set; }
+        public int Id { get; set; }
         public int DirectoryId { get; set; }
         public Directory Directory { get; set; }
 
