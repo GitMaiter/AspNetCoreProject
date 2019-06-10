10fb48a7049d5ceb39a1d1e43f1ccd059ebb1efa
roman-kobzar <roman.kobzar@myhomepay.com>
2019-06-10 19:13:34 +0300

    Add Coffee model to solution

---
+++ AspNetCoreProject/Models/Coffee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreProject.Models
+{
+    public class Coffee
+    {
+        public int Id { get; set; }
+        public string Sort { get; set; }
+        public DateTime StartDate{ get; set; }
+        public DateTime EndDate{ get; set; }
+    }
+}
