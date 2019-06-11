67d5c352bd3688dcc1172ff572b2498c36ef41c5
roman-kobzar <roman.kobzar@myhomepay.com>
2019-06-11 19:53:33 +0300

    Add test middleware for logging

---
+++ AspNetCoreProject/Middlewares/LoggerMiddleware.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace AspNetCoreProject.Middlewares
+{
+    public class LoggerMiddleware
+    {
+        private  readonly RequestDelegate next;
+
+        public LoggerMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if (context.Request.Method == "GET")
+            {
+                // Add logging in to DB here 
+                Debug.WriteLine("Logging GET request");
+                await next.Invoke(context);
+            }
+
+        }
+    }
+}
