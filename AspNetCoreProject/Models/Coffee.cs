1d534e9af10c6f443185d549fa0172d29bc07450
roman-kobzar <roman.kobzar@myhomepay.com>
2019-06-12 19:30:52 +0300

    Update Coffee model

--- AspNetCoreProject/Models/Coffee.cs
+++ AspNetCoreProject/Models/Coffee.cs
@@ -10,6 +10,8 @@ namespace AspNetCoreProject.Models
         public int Id { get; set; }
         public string Sort { get; set; }
         public DateTime StartDate{ get; set; }
-        public DateTime EndDate{ get; set; }
+        public DateTime? EndDate{ get; set; }
+        public double WaterAmount { get; set; }
+        public string Expectation { get; set; }
     }
 }
